255b5340531c335e769866b9501d00ea52626252
claytojcms <joseclaytojcms@gmail.com>
2026-03-24 23:11:32 -0300

    Update q2 insertionsort.c
    É uma questão sobre Selection Sort que pede para mostrar o vetor antes e depois da troca ,posicionamento do elemento selecionado, permitindo observar como o algoritmo altera o vetor a cada etapa da ordenação.

--- q2 insertionsort.c
+++ q2 insertionsort.c
@@ -11,8 +11,7 @@ void imprimir_vetor(int V[],int n){
     void insertionSort(int V[], int n) {
 int i, j;
 int chave;
-int comparacoes=0;
-int movimentacoes=0;
+
 for (i = 1; i < n; i++) {
 chave = V[i];
 j = i;
@@ -21,33 +20,26 @@ imprimir_vetor(V,n);
 
 
 while (j > 0 && chave < V[j - 1]) {
-    comparacoes++;
     V[j] = V[j - 1];
     j--;
-    movimentacoes++;
 }
-    comparacoes++;
     V[j] = chave;
     printf("i=%d, chave=%d | depois:",i,chave);
     imprimir_vetor(V,n);
 }
-printf("movimentacoes:%d\n  comparacoes:%d\n",movimentacoes,comparacoes);
 }
 
 
 
     int main()
     {
-
         int n=6;
-
         int C[]={5,2,4,6,1,3};
-
 
         printf("VETOR C: ANTES \n");
         imprimir_vetor(C,n);
-        printf("DEPOIS: \n");
         insertionSort(C,n);
+         printf("ORDENADO: \n");
         imprimir_vetor(C,n);
 
 
